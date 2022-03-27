5317ff7be3281175b51bab4de084f1e71d500091
Paillet Leo <57637972+Viviny@users.noreply.github.com>
2022-03-27 19:35:43 +0200

    Add files via upload

--- submissions/starting_kit/classifier.py
+++ submissions/starting_kit/classifier.py
@@ -1,16 +1,15 @@
 from sklearn.base import BaseEstimator
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import BaggingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import scale 
 import numpy as np
 
 
 class Classifier(BaseEstimator):
     def __init__(self):
-        self.model = BaggingClassifier(bootstrap=False, max_features=0.6)
+        self.model = LogisticRegression(max_iter=1000)
 
     def fit(self, X, Y):
-        self.model.fit(X, Y)
+        self.model.fit(X,Y)
 
     def predict_proba(self, X):
         res = self.model.predict_proba(X)
